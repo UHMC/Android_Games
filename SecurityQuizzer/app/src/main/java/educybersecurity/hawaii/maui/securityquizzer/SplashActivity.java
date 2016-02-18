b449a1ede4de3874d201b210136ea2b16159a3cc
Mario Canul <mcanul@hawaii.edu>
2016-02-18 13:27:48 -1000

    Add preference file
    - Commented out launching code
    - Toast notification to see if application has ran

--- SecurityQuizzer/app/src/main/java/educybersecurity/hawaii/maui/securityquizzer/SplashActivity.java
+++ SecurityQuizzer/app/src/main/java/educybersecurity/hawaii/maui/securityquizzer/SplashActivity.java
@@ -1,12 +1,16 @@
 package educybersecurity.hawaii.maui.securityquizzer;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 //import java.lang.Thread;
 
 /**
@@ -17,6 +21,7 @@ public class SplashActivity extends AppCompatActivity {
      * Some older devices need a small delay between UI widget updates
      * and a change of the status and navigation bar.
      */
+
     private static final int UI_ANIMATION_DELAY = 300;
     private static final int SPLASH_DELAY=1000;
     private final Handler mHideHandler = new Handler();
@@ -64,9 +69,39 @@ public class SplashActivity extends AppCompatActivity {
         mHideHandler.postAtFrontOfQueue(mHideRunnable);
         //CHECK IF FIRST TIME RUNNING
         //SPLASH DELAY
+
         /*try{Thread.sleep(SPLASH_DELAY);} //bad idea -- no sleeping the UI thread
         catch(InterruptedException e){e.printStackTrace();}*/
-        mHideHandler.postDelayed(new Runnable(){
+
+        SharedPreferences config = getPreferences(MODE_PRIVATE);
+        boolean hasRun = config.getBoolean("hasRun",false);
+
+        // First time run shows preference file as false, running this block
+        if(hasRun == false) {
+            Context context = getApplicationContext();
+            CharSequence text = "First time running this application!";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            // Stage changes to preference file to true
+            SharedPreferences.Editor editor = config.edit();
+            editor.putBoolean("hasRun", true);
+            // Commit them
+            editor.commit();
+        }
+
+        // If preference file shows true, runs this block instead
+        if(hasRun == true) {
+            Context context = getApplicationContext();
+            CharSequence text = "Application already ran!";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+        /*mHideHandler.postDelayed(new Runnable(){
+
             @Override
             public void run(){
                 //IF FIRST TIME, OPEN TUTORIAL-START ACTIVITY
@@ -77,6 +112,18 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         },SPLASH_DELAY);
+
+
+        try{Thread.sleep(2000);}
+        catch(InterruptedException e){}
+        finish(); */
+
+        mHideHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 2000);
     }
 
     private void hide() {
