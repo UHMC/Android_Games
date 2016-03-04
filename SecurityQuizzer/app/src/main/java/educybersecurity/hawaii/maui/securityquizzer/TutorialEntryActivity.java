753a814b7d589117e5412ad45b074876916539ad
Saxon Knight <knight7@hawaii.edu>
2016-03-03 16:30:40 -1000

    Revert "Extend ViewPager class to prevent touch intercepts"
    This reverts commit eff3c0310a59233aad871732b760943102c45036.

--- SecurityQuizzer/app/src/main/java/educybersecurity/hawaii/maui/securityquizzer/TutorialEntryActivity.java
+++ SecurityQuizzer/app/src/main/java/educybersecurity/hawaii/maui/securityquizzer/TutorialEntryActivity.java
@@ -1,6 +1,5 @@
 package educybersecurity.hawaii.maui.securityquizzer;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -10,7 +9,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.AttributeSet;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.LayoutInflater;
@@ -35,7 +33,7 @@ public class TutorialEntryActivity extends AppCompatActivity {
     /**
      * The {@link ViewPager} that will host the section contents.
      */
-    private InterceptedViewPager mViewPager;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +47,7 @@ public class TutorialEntryActivity extends AppCompatActivity {
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         // Set up the ViewPager with the sections adapter.
-        mViewPager = new InterceptedViewPager((ViewPager) findViewById(R.id.container));
+        mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
         makeFullscreen();
@@ -75,19 +73,6 @@ public class TutorialEntryActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public class InterceptedViewPager extends ViewPager{
-        public InterceptedViewPager(Context context){super(context);}
-        public InterceptedViewPager(Context context, AttributeSet attrs){super(context, attrs);}
-        public InterceptedViewPager(ViewPager vp){
-            super(vp.getContext());
-        }
-        @Override
-        public boolean onInterceptTouchEvent(MotionEvent ev) {
-            super.onInterceptTouchEvent(ev); // Called anyway to be safe.
-            return false; // Don't let the ViewPager intercept.
-        }
     }
 
     /**
@@ -160,6 +145,7 @@ public class TutorialEntryActivity extends AppCompatActivity {
                             Log.d("GESTURE", "Flung.");
                             return true;
                         }
+
                         return false;
                     }
                 });
